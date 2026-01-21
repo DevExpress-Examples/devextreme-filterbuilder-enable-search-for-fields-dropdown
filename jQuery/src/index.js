16cdad01611f04991e0fabc652dfe4ff45a45ebc
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-01-21 14:11:00 +0800

    jQuery example done

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,35 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+   DevExpress.ui.dxTreeView.defaultOptions({ 
+    device: { deviceType: 'desktop' },
+    options: {
+        onInitialized: (e) => {
+          const comp = e.component;
+          if (comp.option().cssClass.includes('dx-filterbuilder-fields')) {
+            comp.option('searchEnabled', true);
+          }
+        }
+    	}
+	});
+
+  $('#filterBuilder').dxFilterBuilder({
+    fields,
+    value: filter,
+  });
+
+  $('#apply').dxButton({
+    text: 'Apply Filter',
+    type: 'default',
+    onClick() {
+      const filter = $('#filterBuilder').dxFilterBuilder('instance').option('value');
+      $('#dataGrid').dxDataGrid('instance').option('filterValue', filter);
     },
+  });
+
+  $('#dataGrid').dxDataGrid({
+    columns,
+    showBorders: true,
+    dataSource: products,
+    filterValue: filter,
+    height: 300,
   });
 });
