2709db45089ff68ccfeb66f01c2badb089968079
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-02-19 14:34:19 +0800

    React sample done

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,18 +1,39 @@
 import { useCallback, useState } from 'react';
 import './App.css';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import FilterBuilder, { type FilterBuilderTypes } from 'devextreme-react/filter-builder';
+import dxTreeView, {  type Properties as dxTreeViewProperties, type InitializedEvent as dxTreeViewInitializedEvent  } from "devextreme/ui/tree_view";
 
-function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+import { filter, fields } from './data.ts';
+
+dxTreeView.defaultOptions<dxTreeViewProperties>({
+  device: { deviceType: 'desktop' },
+  options: {
+    onInitialized: (e: dxTreeViewInitializedEvent) => {
+      const comp = e.component;
+      if ((comp?.option() as any).cssClass.includes('dx-filterbuilder-fields')) {
+        comp?.option('height', 200);
+        comp?.option('width', 200); 
+        comp?.option('searchEnabled', true);
+      }
+    }
+  }
+});
+
+const App = () => {
+  const [value, setValue] = useState(filter);
+
+  const onValueChanged = useCallback((e: FilterBuilderTypes.ValueChangedEvent) => {
+    setValue(e.value);
+  }, []);
+
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
+    <div className='demo-container'>
+      <div className='filter-container'>
+        <FilterBuilder fields={fields} value={value} onValueChanged={onValueChanged} />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
