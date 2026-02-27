a22e7ec4788a6be26b227bcd04732ec66ffa6919
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-02-27 18:05:12 +0800

    Add ASP.NET Core sample

--- ASP.NET Core/Models/SampleOrder.cs
+++ ASP.NET Core/Models/SampleOrder.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace ASP_NET_Core.Models;
 public class SampleOrder {
-    public int OrderID { get; set; }
-    public DateTime OrderDate { get; set; }
-    public string CustomerID { get; set; }
-    public string CustomerName { get; set; }
-    public string ShipCountry { get; set; }
-    public string ShipCity { get; set; }
+    public int Product_ID { get; set; }
+    public string Product_Name { get; set; }
+    public decimal Product_Cost { get; set; }
+    public decimal Product_Sale_Price { get; set; }
+    public decimal Product_Retail_Price { get; set; }
+    public int Product_Current_Inventory { get; set; }
+    public string Product_Category { get; set; }
+    public string Product_Brand { get; set; }
+    public string Product_SKU { get; set; }
+    public string Product_Supplier { get; set; }
+    public decimal Product_Weight { get; set; }
+    public DateTime Product_Created_Date { get; set; }
+    public DateTime Product_Last_Updated { get; set; }
 }
