49668e85be8a7f9b16ba5707e25fb281d563a407
Paul Jeremy Manuel <peejheymanuel@gmail.com>
2026-03-04 16:42:20 +0800

    fix eslint, packages, and lint errors

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -3,15 +3,16 @@ import { ref } from 'vue';
 
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
 import DxFilterBuilder from 'devextreme-vue/filter-builder';
-import TreeView, { type Properties as TreeViewProperties, type InitializedEvent as TreeViewInitializedEvent } from 'devextreme/ui/tree_view';
+import TreeView from 'devextreme/ui/tree_view';
+import type { Properties, InitializedEvent } from 'devextreme/ui/tree_view';
 import { filter, fields } from '../data';
 
 const filterValue = ref(filter);
 
-TreeView.defaultOptions<TreeViewProperties>({
+TreeView.defaultOptions<Properties>({
   device: { deviceType: 'desktop' },
   options: {
-    onInitialized: (e: TreeViewInitializedEvent) => {
+    onInitialized: (e: InitializedEvent) => {
       const treeViewInstance = e.component as TreeView;
 
       if ((treeViewInstance?.option('cssClass') as string)?.includes('dx-filterbuilder-fields')) {
